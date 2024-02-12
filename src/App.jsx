52a7a9de4ad6bcfcaa1ed95154be419d73618f36
Sinchana-sl <sinchanaslnaidu1610@gmail.com>
2024-02-12 19:13:42 +0530

    My first very basic and simple React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  const handleOnclick = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3 style={{textAlign:"center"}}>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button type="button" className="btn btn-block" onClick={handleOnclick}>
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
